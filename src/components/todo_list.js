9772f8d7511b27324b3b581fe3dae5380c3ea9cb
NEUS GARCIA <neuspiano@gmail.com>
2022-09-03 20:54:33 +0200

    add toggle functionality

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -6,20 +6,8 @@ import Checkbox from "./checkbox.js";
 
 export default function TodoList({ props }) {
     const [tasks, setTasks] = useState(props.tasks);
-    const [todos, setTodos] = useState(props.tasks[0]);
-    const [done, setDone] = useState(props.tasks[1]);
 
-    const getTask = (id, arr) => {
-        const filteredArr = arr.filter((x) => x.id === id);
-        return filteredArr[0];
-    };
-
-    /* const removeTask = (id, arr) => {
-        const newTodos = arr.filter((todo) => todo.id !== id);
-        setTodos(newTodos);
-    }; */
-
-    console.log("tasks", tasks, "todos", todos, "done", done);
+    console.log("tasks", tasks);
 
     const handleDelete = (id) => {
         let newArr = [];
@@ -27,34 +15,23 @@ export default function TodoList({ props }) {
             const newTasks = tasks[i].filter((task) => task.id !== id);
             newArr.push(newTasks);
         }
-        console.log("new array", arr);
         setTasks(newArr);
     };
 
     const toggleChecked = (id) => {
-        const newTodos = todos.filter((x) => {
-            if (x.id === id) {
-                x.checked = !x.checked;
-                return x;
-            }
-            return x;
-        });
-        setTodos(newTodos);
-    };
-    /*  const task = getTask(id);
-        console.log(task);
-
-        if (task.id) {
-            task.checked = false;
-            const newDone = done.unshift(task);
-            setDone(newDone);
-            return;
+        let newArr = [];
+        for (let i = 0; i < tasks.length; i++) {
+            const newTasks = tasks[i].map((task) => {
+                if (task.id === id) {
+                    task.checked = !task.checked;
+                    return task;
+                }
+                return task;
+            });
+            newArr.push(newTasks);
         }
-        task.checked = true;
-        const newTodos = todos.unshift(task);
-        setTodos(newTodos);
-        return;
-    }; */
+        setTasks(newArr);
+    };
 
     return (
         <div id="todo_list">
